bf8b112de34ea6144e1e84f7bf0132fe5ecb5a2b
xenia <ksen.novikova@mail.ru>
2022-10-13 23:48:01 +0300

    задание сделано 1 2 3 4
    (cherry picked from commit d0da0b50699538cb76220655f9bd55d022dd2290)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = generateRandomArray();
+        int sum=0;
+        int max=0;
+        int min=arr[0];
+        int mediana=sum/arr.length;
+        for (int i=0; i< arr.length; i++){
+            System.out.print(arr[i]+" ");
+            sum=sum+arr[i];
+            if (arr[i]>max){
+                max=arr[i];
+            }
+            if (arr[i]<min){
+                min=arr[i];
+            }
+             mediana=sum/arr.length;
+        }
+        System.out.println("\n Сумма в конце месяца: " + sum);
+        System.out.println(" Максимальное значение: " + max);
+        System.out.println(" Минимальное значение: "+ min);
+        System.out.println(" Среднее значение "+ mediana);
+
+        //задание 4
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        char[]fullName=new char[reverseFullName.length];
+        int over= reverseFullName.length-1;;
+        for (int  i=0;i<reverseFullName.length;i++){
+           fullName[i]=reverseFullName[over-i];
+            System.out.print(fullName[i]);
+        }
     }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
 }
