70d530186faa4dfcd08dea47b0b9cc6218f35818
xenia <ksen.novikova@mail.ru>
2022-10-24 22:06:20 +0300

    задание переделано 1 2 3 4

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,35 @@
 public class Main {
     public static void main(String[] args) {
         int[] arr = generateRandomArray();
-        int sum=0;
-        int max=0;
-        int min=arr[0];
-        int mediana=0;
-        for (int i=0; i< arr.length; i++){
-            System.out.print(arr[i]+" ");
-            sum=sum+arr[i];
-            if (arr[i]>max){
-                max=arr[i];
+        int sum = 0;
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+            sum = sum + arr[i];
+            if (arr[i] > max) {
+                max = arr[i];
             }
-            if (arr[i]<min){
-                min=arr[i];
+            if (arr[i] < min) {
+                min = arr[i];
             }
-            mediana=sum/arr.length;
+
         }
         System.out.println("\n Сумма в конце месяца: " + sum);
         System.out.println(" Максимальное значение: " + max);
-        System.out.println(" Минимальное значение: "+ min);
-        System.out.println(" Среднее значение "+ mediana);
+        System.out.println(" Минимальное значение: " + min);
+        System.out.println(" Среднее значение " + sum / arr.length);
 
         //задание 4
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        char[]fullName=new char[reverseFullName.length];
-        int over= reverseFullName.length-1;
-        for (int  i=0;i<reverseFullName.length;i++){
-            fullName[i]=reverseFullName[over-i];
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        char[] fullName = new char[reverseFullName.length];
+        int over = reverseFullName.length - 1;
+        for (int i = 0; i < reverseFullName.length; i++) {
+            fullName[i] = reverseFullName[over - i];
             System.out.print(fullName[i]);
         }
     }
+
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
